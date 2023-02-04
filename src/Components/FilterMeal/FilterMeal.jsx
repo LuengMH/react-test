e5eec3931ab78aaf0e0701346e42574ae9e400d6
liangmaohong <1216243405@qq.com>
2023-02-04 19:23:19 +0800

    fix

--- src/Components/FilterMeal/FilterMeal.jsx
+++ src/Components/FilterMeal/FilterMeal.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import classes from './filterMeal.module.css';
@@ -6,20 +6,18 @@ import classes from './filterMeal.module.css';
 
 // 输入文字触发事件
 const inputChangeHandler = (e, setKeyword) => {
-    // console.log(e.target.value);
     const keyWord = e.target.value.trim();
     setKeyword(keyWord);
 }
 
 // 点击搜索触发事件
-const iconCclickHandler = (keyWord, onFilter) => {
+const iconClickHandler = (keyWord, onFilter) => {
     onFilter(keyWord);
 }
 
 // 点击回车时，触发事件
 const keyDownHandler = (e, keyWord, onFilter) => {
     if (e.keyCode === 13) {
-        // console.log(keyWord)
         onFilter(keyWord);
     }
 }
@@ -28,10 +26,24 @@ const FilterMeal = ({
     onFilter
 }) => {
     const [keyWord, setKeyword] = useState('');
+
+    useEffect(() => {
+        const timer = setTimeout(()=> {
+            // console.log(444)
+            onFilter(keyWord);
+        }, 1000)
+
+        
+        return () => {
+            // console.log('1111')
+            clearTimeout(timer);
+        }
+    }, [keyWord])
     return (
         <div className={classes.FilterWrap}>
             <div className={classes.inputOuter}>
                 <input
+                    value={keyWord}
                     placeholder='请输入关键字'
                     className={classes.input}
                     onChange={(e) => inputChangeHandler(e, setKeyword)}
@@ -40,7 +52,7 @@ const FilterMeal = ({
                 <FontAwesomeIcon
                     icon={faSearch}
                     className={classes.icon}
-                    onClick={() => iconCclickHandler(keyWord, onFilter)}
+                    onClick={() => iconClickHandler(keyWord, onFilter)}
                     />
             </div>
         </div>
